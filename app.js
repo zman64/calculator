ee010543464e1b0ad02f111c2dc41e7913a64c48
Zachary Kahlig <zkahlig@AmpPartners.Org>
2022-05-27 16:24:58 -0400

    add js function to populate display with numbers

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const digit1 = document.querySelector('.digit-1');
+const display = document.querySelector('.display__content');
+
+
 function add(...arr) {
     return arr.reduce((total, currentItem) => {
         return total + currentItem;
@@ -25,3 +29,12 @@ function divide(...arr) {
 function operate(operator, ...numbers) {
     return operator(...numbers)
 }
+
+function popDisplay() {
+
+}
+
+digit1.addEventListener('click', function(e){
+    console.log(e.target.value);
+    display.textContent += e.target.value
+})
