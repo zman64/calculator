fd4c164a89181529d3ea725f7bf082da949a762e
Zachary Kahlig <zkahlig@AmpPartners.Org>
2022-05-31 16:28:05 -0400

    Work on adding functions to populate displays

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const digit1 = document.querySelector('.digit-1');
-const display = document.querySelector('.display__content');
+const digit2 = document.querySelector('.digit-2');
+const digit3 = document.querySelector('.digit-3');
+const digit4 = document.querySelector('.digit-4');
+const digit5 = document.querySelector('.digit-5');
+const digit6 = document.querySelector('.digit-6');
+const digit7 = document.querySelector('.digit-7');
+const digit8 = document.querySelector('.digit-8');
+const digit9 = document.querySelector('.digit-9');
+const digit0 = document.querySelector('.digit-0');
 
+const digit = document.querySelectorAll('.digit')
+console.log(digit)
+const display = document.querySelector('.display__content');
+const clear = document.querySelector('.clear');
+
+let displayValue = 0;
 
 function add(...arr) {
     return arr.reduce((total, currentItem) => {
@@ -34,7 +48,20 @@ function popDisplay() {
 
 }
 
-digit1.addEventListener('click', function(e){
-    console.log(e.target.value);
-    display.textContent += e.target.value
+for(let i = 0; i < 10; i++){
+    `digit${i}`.addEventListener('click', function(e){
+    display.textContent += e.target.value;
+    displayValue += e.target.value;
+    })
+}
+
+// digit1.addEventListener('click', function(e){
+//     // console.log(e.target.value);
+//     display.textContent += e.target.value;
+//     displayValue += e.target.value;
+// });
+
+clear.addEventListener('click', function(e){
+    display.textContent = '';
+    displayValue = 0;
 })
