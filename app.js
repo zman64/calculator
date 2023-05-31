9e711c8e2245495f4ba66c1940dc0d2a944dec70
Zachary Kahlig <zkahlig64@gmail.com>
2023-05-31 16:32:57 -0400

    Improve state logic for calculator code

--- app.js
+++ app.js
@@ -4,58 +4,59 @@ class Calculator {
         this.currentInput = '';
         this.operator = null;
         this.firstInput = null;
-
-        
     }
 
-    updateDisplay(value) {
+    updateDisplay() {
         this.displayElement.textContent = this.currentInput;
     }
 
-    processDigit(e) {
-        // similar logic as your digit event handler...
+    appendNumber(number) {
+        this.currentInput = this.currentInput.toString() + number.toString();
+        this.updateDisplay();
     }
 
-    processOperator(e) {
-        // similar logic as your operator event handler...
+    chooseOperation(operation) {
+        if (this.currentInput === '') return;
+        if (this.firstInput !== null) {
+            this.compute();
+        }
+        this.operator = operation;
+        this.firstInput = this.currentInput;
+        this.currentInput = '';
+    }
+    
+    compute() {
+        let result;
+        const prev = parseFloat(this.firstInput);
+        const current = parseFloat(this.currentInput);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operator) {
+            case '+':
+                result = current + prev;
+                break;
+            case '-':
+                result = prev - current;
+                break;
+            case '*':
+                result = current * prev;
+                break;
+            case '/':
+                result = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentInput = result;
+        this.operator = null;
+        this.firstInput = null;
+        this.updateDisplay();
     }
 
-    processEquals(e) {
-        // similar logic as your equals event handler...
-    }
-
-    processClear(e) {
-        // similar logic as your clear event handler...
-    }
-
-    add(...arr) {
-        return arr.reduce((total, currentItem) => {
-            return total + currentItem;
-        }, 0)
-    }
-
-    subtract(...arr) {
-        return arr.reduce((total, currentItem) => {
-            return total - currentItem;
-        })
-    }
-
-    multiply(...arr) {
-        return arr.reduce((total, currentItem) => {
-            return total * currentItem
-        }, 0)
-    }
-
-    divide(...arr) {
-        return arr.reduce((total, currentItem) => {
-            return total / currentItem
-        });
-    }
-
-
-
-    operate(operator, ...numbers) {
-        return operator(...numbers)
+    clear() {
+        this.currentInput = '';
+        this.operator = null;
+        this.firstInput = null;
+        this.updateDisplay();
     }
 }
 
@@ -68,131 +69,46 @@ const functions = document.querySelectorAll('.functions');
 const clear = document.querySelector('.clear');
 const equals = document.querySelector('.equals');
 
-digits.forEach(digit => {
-    digit.addEventListener('click', (e) => calculator.processDigit(e));
+document.querySelectorAll('.digit').forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.textContent);
+    });
 });
 
-functions.forEach(operator => {
-    operator.addEventListener('click', (e) => calculator.processOperator(e));
+document.querySelectorAll('.functions').forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.textContent);
+    });
 });
 
-equals.addEventListener('click', (e) => calculator.processEquals(e));
+document.querySelector('.equals').addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
 
-clear.addEventListener('click', (e) => calculator.processClear(e));
-
-
-// const digits = document.querySelectorAll('.digit');
-// const functions = document.querySelectorAll('.functions');
-// const display = document.querySelector('.display__content');
-// const clear = document.querySelector('.clear');
-// const equals = document.querySelector('.equals');
-
-// let input1 = 0;
-// let input2 = 0;
-// let operateString;
-// let calculatedValue = 0;
-// display.textContent = 0;
-// let isFunctionClicked = false;
-// let hitTwice = false;
-// let isEqualsClicked = false;
-
-// digits.forEach(digit => {
-//     digit.addEventListener('click', function (e) {
-//         if (isFunctionClicked) {
-//             // display.textContent = null;
-//             input2 += e.target.value
-//             input2 = input2.replace(/^0+/, '');
-//             display.textContent = input2;
-//             // isFunctionClicked = false;
-//             hitTwice = true;
-//         }  else {
-//             // if(isEqualsClicked) display.te
-//             console.log(input1)
-//             input1 += e.target.value;
-//             input1 = input1.replace(/^0+/, '');
-//             display.textContent = input1;
-//             isFunctionClicked = false;
-//         }
-//     })
-// })
-
-// functions.forEach(operator => {
-//     operator.addEventListener('click', function (e) {
-//         isFunctionClicked = true;
-//         // display.textContent = input1;
-//         let symbol = e.target.value;
-//         if (symbol === '+') {
-//             operateString = add;
-
-//         } else if (symbol === '-') {
-//             operateString = 'subtract';
-//         }
-
-//         if (hitTwice) {
-//             // isFunctionClicked = false;
-//             input2 = parseInt(input2);
-//             input1 = parseInt(input1);
-//             console.log(input1);
-//             console.log(input2);
-//             calculatedValue = operate(operateString, input1, input2);
-//             display.textContent = calculatedValue;
-//             input1 = calculatedValue;
-//             input2 = 0;
-
-//         }
-//     })
-// })
-
-// equals.addEventListener('click', function (e) {
-//     hitTwice = false;
-//     if (isFunctionClicked) {
-//         input2 = parseInt(input2);
-//         input1 = parseInt(input1);
-//         calculatedValue = operate(operateString, input1, input2);
-//         isFunctionClicked = false;
-//         input1 = parseInt(calculatedValue);
-//         input2 = 0;
-//         display.textContent = calculatedValue;
-//         calculatedValue = 0;
-//         return;
-//     } else {
-//         input1 = 0;
-//         input2 = 0;
-//         isFunctionClicked = false;
+document.querySelector('.clear').addEventListener('click', () => {
+    calculator.clear();
+})
+// add(...arr) {
+//         return arr.reduce((total, currentItem) => {
+//             return total + currentItem;
+//         }, 0)
 //     }
-//     input1 = 0;
 
+//     subtract(...arr) {
+//         return arr.reduce((total, currentItem) => {
+//             return total - currentItem;
+//         })
+//     }
 
-// })
+//     multiply(...arr) {
+//         return arr.reduce((total, currentItem) => {
+//             return total * currentItem
+//         }, 0)
+//     }
 
-// clear.addEventListener('click', function (e) {
-//     hitTwice = false;
-//     isFunctionClicked = false;
-//     display.textContent = 0;
-//     input1 = 0;
-//     input2 = 0;
-// })
-
-
-
-
-
-
-
-function multiply(...arr) {
-
-}
-
-function divide(...arr) {
-
-}
-
-function operate(operator, ...numbers) {
-    // operator = \operator\
-
-}
-
-
-
-
-
+//     divide(...arr) {
+//         return arr.reduce((total, currentItem) => {
+//             return total / currentItem
+//         });
+//     }
